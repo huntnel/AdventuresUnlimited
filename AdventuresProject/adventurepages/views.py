8f3359f79fd652b66fb1702e0f1acafd0d593d4d
Hunter Nelson <dahuntaman38j@gmail.com>
2021-11-01 15:44:11 -0600

    Commit on Nov 1 after meeting with TA

--- AdventuresProject/adventurepages/views.py
+++ AdventuresProject/adventurepages/views.py
@@ -4,10 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 
 def indexPageView(request) :
-    return HttpResponse('This is the index response')
+    sOutput = '<html><head><title>My Title</title></head><body><p style="color:red;"><b>one</b></p><p style="color:blue;">two</p><p style="font-size:50px;">three</p><ul><li>A</li><li>B</li><li>C</li></ul></body></html>'
+    return HttpResponse(sOutput)
 
-def campingPageView(request) :
-    return HttpResponse('This is the camping response')
+def campingPageView(request, trip_name) :
+    sOutput = sOutput = '<html><head><title>My Title</title></head><body><p>Welcome to ' + trip_name + '</p></body></html>'
+    return HttpResponse(sOutput)
 
 def hikingPageView(request) :
     return HttpResponse('This is the hiking response')
