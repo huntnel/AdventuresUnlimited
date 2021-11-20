91174bfe9c73116de4dab79cc0e39d5cdfb54e6c
willpaxton16 <90337820+willpaxton16@users.noreply.github.com>
2021-11-19 17:05:06 -0700

    Admin/Models Commit

--- AdventuresProject/adventurepages/admin.py
+++ AdventuresProject/adventurepages/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Hike, Camping
 # Register your models here.
+
+admin.site.register(Hike)
+admin.site.register(Camping)
--- AdventuresProject/adventurepages/models.py
+++ AdventuresProject/adventurepages/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Hike(models.Model):
+    pass
+
+class Camping(models.Model):
+    pass
