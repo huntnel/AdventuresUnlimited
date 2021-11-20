a125181464137af02191c878ac8eedfd75b389e1
Hunter Nelson <90410647+huntnel@users.noreply.github.com>
2021-11-19 19:41:16 -0700

    CRUD

--- AdventuresProject/adventurepages/urls.py
+++ AdventuresProject/adventurepages/urls.py
@@ -1,8 +1,20 @@
 from django.urls import path
-from .views import indexPageView, campingPageView, hikingPageView
+from .views import indexPageView, storeHikePageView, updateCampPageView, updateHikePageView, storeCampPageView, campingPageView
+from .views import hikingPageView, createHikePageView, createCampPageView, updateHikePageView, updateCampPageView
+from .views import deleteCampPageView, deleteHikePageView
 
 urlpatterns = [
     path("", indexPageView, name = "index"),
     path("camping/", campingPageView, name = "camping"),
     path("hiking/", hikingPageView, name = "hiking"),
+    path("createhike/", createHikePageView, name = "createhike"),
+    path("createcamp/", createCampPageView, name = "createcamp"),
+    path("updatehike/", updateHikePageView, name = "updatehike"),
+    path("updatecamp/", updateHikePageView, name = "updatehike"),
+    path("deletehike/", deleteHikePageView, name = "deletehike"),
+    path("deletehike/", deleteHikePageView, name = "deletehike"),
+    path("storehike/", storeHikePageView, name = "storehike"),
+    path("storecamp/", storeCampPageView, name = "storecamp"),
+    path("updatecamp/", updateCampPageView, name = "updatecamp"),
+    path("updatehike/", updateHikePageView, name = "updatehike"),
 ]
--- AdventuresProject/adventurepages/views.py
+++ AdventuresProject/adventurepages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Hiking, Camping
 
 # Create your views here.
 
@@ -11,4 +12,50 @@ def campingPageView(request) :
 
 def hikingPageView(request) :
     return render(request, 'adventurepages/hiking.html')
+
+def createHikePageView(request) :
+    return render(request, 'adventurepages/createhike.html')
+
+def createCampPageView(request) :
+    return render(request, 'adventurepages/camping.html')
+
+def updateHikePageView(request) :
+    return render(request, 'adventurepages/updatehike.html')
+
+def updateCampPageView(request) :
+    return render(request, 'adventurepages/updatecamp.html')
+
+def deleteHikePageView(request) :
+    return render(request, 'adventurepages/deletehike.html')
+
+def deleteCampPageView(request) :
+    return render(request, 'adventurepages/deletecamp.html')
+
+def storeHikePageView(request) :
+        #Check to see if the form method is a get or post
+    if request.method == 'POST':
+
+        #Create a new employee object from the model (like a new record)
+        new_hike = Hiking()
+        
+        #Store the data from the form to the new object's attributes (like columns)
+        new_hike.hikename = request.POST.get('hike_name')
+        new_hike.elevationgain = request.POST.get('elevation_gain')
+        new_hike.length = request.POST.get('length')
+        new_hike.description = request.POST.get('description')
+        new_hike.bathroom = request.POST.get('bathroom')
+        new_hike.potablewater = request.POST.get('water')
+        new_hike.firepits = request.POST.get('fire')
+        new_hike.address = request.POST.get('address')
+        new_hike.city = request.POST.get('city')
+        new_hike.state = request.POST.get('state')
+        new_hike.zipcode = request.POST.get('zip')
+        new_hike.difficulty = request.POST.get('difficulty')
+                
+        #Save the employee record
+        new_hike.save()
+    return render(request, 'adventurepages/hiking.html')
+
+def storeCampPageView(request) :
+    return render(request, 'travelpages/storecamp.html')
    
