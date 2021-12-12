de6df9c3e77ea26d48b867a60aea01d17a477bcb
Hunter Nelson <90410647+huntnel@users.noreply.github.com>
2021-12-11 19:36:36 -0700

    CRUD done for camps

--- AdventuresProject/adventurepages/urls.py
+++ AdventuresProject/adventurepages/urls.py
@@ -1,17 +1,15 @@
 from django.urls import path
-from .views import indexPageView, storeHikePageView, updateCampPageView, updateHikePageView, storeCampPageView, campingPageView
+from .views import indexPageView, storeHikePageView, updateCampPageView, updateHikePageView, campingPageView
 from .views import hikingPageView, createHikePageView, createCampPageView, updateHikePageView, updateCampPageView
-<<<<<<< HEAD
-from .views import deleteCampPageView, deleteHikePageView, editCampPageView
-=======
-from .views import deleteCampPageView, deleteHikePageView, aboutPageView
->>>>>>> e3b229538e3a74a0ca066b8978ff603f19cb9e49
+from .views import deleteCampPageView, deleteHikePageView, editCampPageView, aboutPageView, storeCampPageView
+
 
 urlpatterns = [
     path("", indexPageView, name = "index"),
     path("about/", aboutPageView, name="about"),
     path("camping/", campingPageView, name = "camping"),
     path("createcamp/", createCampPageView, name = "createcamp"),
+    path("storecamp/", storeCampPageView, name = "storecamp"),
     path("editcamp/<int:campid>/", editCampPageView, name = "editcamp"),
     path("updatecamp/<int:id>/", updateCampPageView, name = "updatecamp"),
     path("deletecamp/<int:id>/", deleteCampPageView, name = "deletecamp"),
